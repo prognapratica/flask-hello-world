f6d356be6b397f74f38cf1228bd4d952bab2ab29
prognapratica <113378192+prognapratica@users.noreply.github.com>
2022-11-01 17:54:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 import random
+import os
 
 @app.route('/')
 def hello_world():
-    return str(random.random())
+    if 'PORT' in os.environ:
+        return 'tem porta'
+return 'no port'
